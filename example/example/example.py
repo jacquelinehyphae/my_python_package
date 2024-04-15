7da178190f477ba974d187c5a83c2a7828cf2bf3
Jacqueline-Kelly <jacqueline@hyphae.net>
2024-04-15 15:21:59 -0700

    multiple files

--- example/example/example.py
+++ example/example/example.py
@@ -1,9 +1,14 @@
 import pandas
+from exampleHelper import subtract_helper
 
 class Example:
   def __init__(self, x=1, y=2):
     self.x = x
     self.y = y
 
-  def exampleMethod(self):
-    return self.y + self.x
+  def add(self):
+    result = self.y + self.x
+    return result
+
+  def subtract(self):
+    return subtract_helper(self.x, self.y)
---
+++ example/example/exampleHelper.py
@@ -0,0 +1,4 @@
+
+def subtract_helper(x, y):
+  result = x - y
+  return result
